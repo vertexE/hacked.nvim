19facd3c7a25b0b225c3daa588160197facb66da
Josiah Denton <jfranklindenton@gmail.com>
2025-03-19 22:43:32 -0400

    chore: move diagnostics to same line

--- lua/hacked/diagnostics.lua
+++ lua/hacked/diagnostics.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+local tbl = require("hacked._private.tbl")
+
 --- used for setting highlight groups
 --- @param severity vim.diagnostic.Severity
 --- @return string
@@ -14,9 +16,10 @@ local severity_to_string = function(severity)
 end
 
 local MAX_DIAGNOSTIC_MSG_LENGTH = 80
+local DEFAULT_SPACES = 5
 
 --- @param diagnostic vim.Diagnostic
---- @param spaces integer number of spaces to prefix
+--- @param spaces integer
 --- @return table
 local format_diagnostic = function(diagnostic, spaces)
     local diagnostic_split = vim.split(diagnostic.message, "\n")
@@ -48,7 +51,7 @@ local format_diagnostic = function(diagnostic, spaces)
     for i, message in ipairs(lines_limited) do
         local symbol
         if i == 1 then
-            symbol = "󱞽"
+            symbol = " "
         else
             symbol = "│"
         end
@@ -63,24 +66,41 @@ local show_virtual_text_diagnostics = function()
     local ns = vim.api.nvim_create_namespace("hacked.diagnostics.diagnostic")
     local bufnr = vim.api.nvim_get_current_buf()
     vim.api.nvim_buf_clear_namespace(bufnr, ns, 0, -1)
-    local cursor_line = vim.api.nvim_win_get_cursor(0)[1] - 1
-
+    local cursor_line = vim.fn.getpos(".")[2] - 1 -- is now zero index based
     local diagnostics = vim.diagnostic.get(bufnr, { lnum = cursor_line, severity = vim.diagnostic.severity.ERROR })
     if #diagnostics == 0 then
         return
     end
+    local longest_line =
+        tbl.max(vim.iter(vim.api.nvim_buf_get_lines(bufnr, cursor_line, cursor_line + #diagnostics, false))
+            :map(function(v)
+                return #v
+            end)
+            :totable())
+
+    local spaces = vim.iter(vim.api.nvim_buf_get_lines(bufnr, cursor_line, cursor_line + #diagnostics, false))
+        :map(function(v)
+            return longest_line - #v
+        end)
+        :totable()
+
     local virt_lines = {}
-    for _, diagnostic in ipairs(diagnostics) do
-        local lines = format_diagnostic(diagnostic, diagnostic.col)
+    for i, diagnostic in ipairs(diagnostics) do
+        local space = spaces[i] or 0
+        local lines = format_diagnostic(diagnostic, space + DEFAULT_SPACES)
         for _, line in ipairs(lines) do
             table.insert(virt_lines, line)
         end
     end
-    local diagnostic = diagnostics[1]
-    vim.api.nvim_buf_set_extmark(bufnr, ns, diagnostic.lnum, 0, {
-        virt_lines = virt_lines,
-        virt_text_pos = "eol",
-    })
+
+    vim.print(virt_lines)
+
+    for i, vline in ipairs(virt_lines) do
+        vim.api.nvim_buf_set_extmark(bufnr, ns, cursor_line + i - 1, 0, {
+            virt_text = vline,
+            virt_text_pos = "eol",
+        })
+    end
 end
 
 -- TODO: add opts for which diagnostics to show + symbols, spacing, etc
